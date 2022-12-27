dea5bd29c74e56be37bdb44244c05566b22da1e6
Anik Dey <deyanik659@gmail.com>
2022-12-27 22:19:29 +0600

    u

--- Program.cs
+++ Program.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 
-class Test{
-
-    static float findarea(float a,float b,float c)
+namespace CGPAcalculator
+{
+    class Program
     {
-        if( a < 0 || b < 0 ||c < 0 ||
-        (a + b <= c) || a+c<=b || b+c<=a)
+        static void Main()
         {
-            Console.Write ("not a valid triangel");
-            System.Environment.Exit(0);
+            double CGPA;
+
+            float first = Single.Parse(Console.ReadLine());
+            float second = Single.Parse(Console.ReadLine());
+            float third = Single.Parse(Console.ReadLine());
+            float fourth = Single.Parse(Console.ReadLine());
+            float fifth = Single.Parse(Console.ReadLine());
+            float sixth = Single.Parse(Console.ReadLine());
+            float seventh = Single.Parse(Console.ReadLine());
+            float eighth = Single.Parse(Console.ReadLine());
+
+            CGPA = (first + second + third + fourth + fifth + sixth + seventh + eighth) / (8);
+
+            Console.WriteLine("Your CGPA is:" + String.Format("{0:0.00}", CGPA));
+
         }
-        float s = (a+b+c)/2;
-        return(float)Math.Sqrt(s*(s-a)*(s-b)*(s-c));
-
     }
-    public static void Main()
-
-    {
-        float a=3.0f;
-        float b=4.0f;
-        float c=5.0f;
-        Console.Write("area is " + findarea(a,b,c));
-    }
-
-
 }
